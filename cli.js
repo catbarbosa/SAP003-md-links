957da1eb819b4a7b91bec3c04a238f0776e26bed
seu-catarinabarbosa <catarina.pinheiro07@gmail.com>
2019-12-03 13:59:48 -0300

    Alteração no cli, para controle de caracterie no text

--- cli.js
+++ cli.js
@@ -2,5 +2,9 @@
 const mdLinks = require("./lib/index");
 
 mdLinks("./README.md")
-  .then(result => console.log(result))
+  .then(
+    result.forEach(item => {
+      console.log(item.href, "-", item.text.substring(0, 50));
+    })
+  )
   .catch(error => console.log(error));
