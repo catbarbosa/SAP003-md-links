9619e1cdda959762bdc84c7a420fef7f9c22e237
seu-catarinabarbosa <catarina.pinheiro07@gmail.com>
2019-12-03 20:34:34 -0300

    Hack Edition Testes refeitos

--- cli.js
+++ cli.js
@@ -1,10 +1,16 @@
 #! /usr/bin/env node
 const mdLinks = require("./lib/index");
+const path = process.argv[2];
+const option= process.argv[3];
 
-mdLinks("./README.md")
-  .then(
+mdLinks(path, option)
+  .then(result =>
     result.forEach(item => {
-      console.log(item.href, "-", item.text.substring(0, 50));
+      if (option) {
+        console.log(item.text, item.link, item.status);
+      } else {
+        console.log(item.text, item.link);
+      }
     })
   )
   .catch(error => console.log(error));
